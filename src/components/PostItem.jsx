6885484c2f75851846a24f17c3f5197212cbc788
reddyarun59 <reddyarun59@yahoo.com>
2022-06-02 00:39:28 +0530

    Added posts

---
+++ src/components/PostItem.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const PostItem = ({post, id}) => {
+  return (
+    <li>
+        <div>
+            <div>
+                <Link to={`/profile/${post.userId}`}>
+                    <h1>{post.firstName} {post.lastName}</h1>
+                    <h2>@{post.username}</h2>
+                </Link>
+            </div>
+            <div>
+                <Link to={`/post/${id}`}>
+                    <img src={post.profileImage[0]} alt="profileImage"/>
+                    <h1>{post.content}</h1>
+                </Link>
+            </div>
+            <div>
+                <span>{post.likes.likeCount}</span>
+            </div>
+
+        </div>
+    </li>
+  )
+}
+
+export default PostItem
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,56 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import { collection, getDocs, query, where, orderBy, limit, startAfter } from 'firebase/firestore'
+import { useParams } from 'react-router-dom'
+import { db } from '../firebase.config'
+import { toast } from 'react-toastify'
+import PostItem from "../components/PostItem"
 
 const Home = () => {
+  const [posts, setPosts]=useState(null)
+  const [loading, setLoading] = useState(true)
+
+  const params=useParams()
+
+  useEffect(() =>{
+    const fetchPosts=async()=>{
+      try {
+        //Get Reference
+        const postsRef=collection(db, "posts")
+
+        //Create a query for the posts
+        //const q= query(postsRef)
+
+        //Execute query
+        const querySnap=await getDocs(postsRef)
+
+        const posts=[]
+        querySnap.forEach((doc)=>{
+          return posts.push({
+            id:doc.id,
+            data:doc.data()
+          })
+        })
+
+        setPosts(posts)
+        setLoading(false)
+      } catch (error) {
+        toast.error("Could not fetch posts")
+      }
+    }
+    fetchPosts()
+  },[])
   return (
-    <div>Home</div>
+    <div>
+      <header>
+        <h1>Feed</h1>
+      </header>
+      {loading?<h1>Loading</h1>:<div>
+        {posts.map(post=>(
+          <PostItem post={post.data} id={post.id} key={post.id}/>
+        ))}
+      </div>}
+      
+    </div>
   )
 }
 
